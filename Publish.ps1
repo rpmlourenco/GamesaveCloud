6811855edd56bd83348ce931ae207ec3ba061875
Rui Lourenço <rpmlourenco@gmail.com>
2023-04-16 18:20:02 +0200

    Publish version 1.0.0.0

--- Publish.ps1
+++ Publish.ps1
@@ -1,5 +1,6 @@
 dotnet publish -p:PublishProfile=FolderProfile -p:Configuration=Release .\GamesaveCloudCLI\GamesaveCloudCLI.csproj
 $version = [System.Diagnostics.FileVersionInfo]::GetVersionInfo(".\GamesaveCloudCLI\bin\Release\net7.0\publish\win-x64\GamesaveCloudCLI.exe").FileVersion
+Remove-Item '.\GamesaveCloudCLI\bin\Release\net7.0\publish\win-x64\*' -Include GamesaveCloudCLI*.zip
 Compress-Archive -Force -Path .\GamesaveCloudCLI\bin\Release\net7.0\publish\win-x64\*.* -DestinationPath .\published\GamesaveCloudCLI-$version.zip
 git add -A
 git commit -a -m "Publish version $version"
